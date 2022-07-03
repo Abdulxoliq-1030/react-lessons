0c901ea67bd595892d31a7075aef303823505272
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-03 18:56:47 +0500

    Destructing

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import Counter from "./components/lesson-6-setState/Counter";
+import DesClass from "./components/lesson-7-destructoring-props-state/DesClass";
+import Destructing from "./components/lesson-7-destructoring-props-state/Destructing";
+
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Destructing name="Diana" heroName="Wonder Women" />
+      <DesClass name="Bruce" heroName="Batman" />
     </div>
   );
 }
