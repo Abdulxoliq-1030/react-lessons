545f48a409f4a2623f4873c27f34bcd16e95986a
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-03 16:26:17 +0500

    State

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
-import HelloProps from "./components/lesson-4-props/HelloProps";
-import Welcome from "./components/lesson-2-class-components/Welcome";
+import Message from "./components/lesson-5-state/Message";
 function App() {
   return (
     <div className="App">
-      <HelloProps name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </HelloProps>
-      <HelloProps name="Alice" heroName="Superman">
-        <button>Action</button>
-      </HelloProps>
-      <HelloProps name="Jess" heroName="Wonder Woman" />
-
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Alice" heroName="Superman" />
-      <Welcome name="Jess" heroName="Wonder Woman"/>
+      <Message />
     </div>
   );
 }
