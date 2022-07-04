df2bd428042b4f63bf456e7a1279fb72cbccc3f8
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-04 21:58:50 +0500

    Form handling

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-import Inline from "./components/lesson-13-styles/Inline";
-import Stylesheet from "./components/lesson-13-styles/Stylesheet";
-import "./components/lesson-13-styles/appStyles.css";
-import styles from "./components/lesson-13-styles/appStyles.module.css";
+import Form from "./components/lesson-14-form-handling/Form";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1>
-      <Stylesheet primary={true} />
-      <Inline />
+      <Form />
     </div>
   );
 }
