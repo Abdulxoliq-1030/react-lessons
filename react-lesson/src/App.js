9b6a3959f27ab0380b92613a6f5d5c47c117a78a
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-04 18:25:51 +0500

    Conditional rendering

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import ParentComp from "./components/lesson-10-methods-as-props/ParentComp";
-import EventBind from "./components/lesson-9-event-bind/EventBind";
+import UserGreeting from "./components/lesson-11-conditional-rendering-operators/UserGreeting";
 
 function App() {
   return (
     <div className="App">
-      <ParentComp />
+      <UserGreeting />
     </div>
   );
 }
