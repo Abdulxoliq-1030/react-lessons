959d6244115b047377b425b20d835af04868e7bf
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-05 21:31:08 +0500

    Component updating lifecycle methods

--- react-lesson/src/components/lesson-15-component-mounting-lifecycle-methods/LifecycleA.js
+++ react-lesson/src/components/lesson-15-component-mounting-lifecycle-methods/LifecycleA.js
@@ -20,11 +20,32 @@ class LifecycleA extends Component {
     console.log("LifecycleA componentDidMount");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifecycleA shouldComponentUpdate");
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps,prevState) {
+   console.log("LifecycleA getSnapshotBeforeUpdate");
+   return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifecycleA componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({
+        name: 'Codevolution'
+    })
+  }
+
   render() {
     console.log("Lifecycle A render");
     return (
       <>
         <div> Lifecycle A</div>
+        <button onClick={this.changeState}>Change state</button>
         <LifecycleB />
       </>
     );
--- react-lesson/src/components/lesson-15-component-mounting-lifecycle-methods/LifecycleB.js
+++ react-lesson/src/components/lesson-15-component-mounting-lifecycle-methods/LifecycleB.js
@@ -20,6 +20,20 @@ import React, { Component } from 'react'
         console.log('LifecycleB componentDidMount');
     }
 
+    shouldComponentUpdate() {
+        console.log("LifecycleB shouldComponentUpdate");
+        return true
+      }
+    
+      getSnapshotBeforeUpdate(prevProps,prevState) {
+       console.log("LifecycleB getSnapshotBeforeUpdate");
+       return null;
+      }
+    
+      componentDidUpdate() {
+        console.log("LifecycleB componentDidUpdate");
+      }
+
   render() {
     console.log('Lifecycle B render')
     return (
