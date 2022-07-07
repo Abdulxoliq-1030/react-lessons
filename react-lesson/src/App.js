b80fca172ab25278492a1df2171f957d1db9996b
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-07 18:54:44 +0500

    Refs with Class components

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import FocusInput from "./components/lesson-18-refs/FocusInput";
 import RefsDemo from "./components/lesson-18-refs/RefsDemo";
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo/>
+      {/* <RefsDemo/> */}
+      <FocusInput />
     </div>
   );
 }
