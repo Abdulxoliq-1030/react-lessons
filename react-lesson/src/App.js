f65d8a9a575163f9f138a3fec22c2790b93eade2
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-07-15 21:30:10 +0500

    react hook introduction

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import PostForm from "./components/lesson-23-http-requests/PostForm";
-import Postlist from "./components/lesson-23-http-requests/Postlist";
 
 function App() {
   return (
     <div className="App">
-      {/* <Postlist /> */}
-      <PostForm />
+     
     </div>
   );
 }
